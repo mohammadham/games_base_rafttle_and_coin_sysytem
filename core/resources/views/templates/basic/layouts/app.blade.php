d5dfe6eeb6da9c8ef26d05cdda553041d0738991
Mohammad <mmm.2015.ma60@gmail.com>
2026-01-03 00:59:19 +0330

    banner section

--- core/resources/views/templates/basic/layouts/app.blade.php
+++ core/resources/views/templates/basic/layouts/app.blade.php
@@ -13,13 +13,18 @@
 
     @include('partials.seo')
 
+    @if($isRtl)
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.rtl.min.css">
+    @else
     <link rel="stylesheet" href="{{ asset('assets/global/css/bootstrap.min.css') }}">
+    @endif
     <link rel="stylesheet" href="{{ asset('assets/global/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/global/css/line-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/slick.css') }}" />
     <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/main.css') }}" />
     <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/custom.css') }}">
     @if(in_array(session('lang', config('app.locale')), ['fa', 'ar', 'he', 'ur', 'ps', 'ku']))
+    <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/rtl-fixes.css') }}">
     <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css" rel="stylesheet" type="text/css" />
     @endif
 
