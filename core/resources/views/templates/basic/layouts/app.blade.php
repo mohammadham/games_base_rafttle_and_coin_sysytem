11701ebd7ce54c10a85811af12b92f8e7a311f8f
Mohammad <mmm.2015.ma60@gmail.com>
2026-01-07 19:16:28 +0330

    rtl problem

--- core/resources/views/templates/basic/layouts/app.blade.php
+++ core/resources/views/templates/basic/layouts/app.blade.php
@@ -24,8 +24,8 @@
     <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/main.css') }}" />
     <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/custom.css') }}">
     @if(in_array(session('lang', config('app.locale')), ['fa', 'ar', 'he', 'ur', 'ps', 'ku']))
-    <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/rtl-fixes.css') }}">
     <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/rtl-fixes.css') }}">
     @endif
 
     @stack('style-lib')
